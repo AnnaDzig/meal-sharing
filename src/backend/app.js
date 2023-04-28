0a9e4345cf8ce905d4bcfb2f90eff1ed3598f757
AnnaDzig <adzyhota@protonmail.com>
2023-04-28 14:01:57 +0200

    add respond tasks

--- src/backend/app.js
+++ src/backend/app.js
@@ -21,6 +21,77 @@ app.use(cors());
 
 router.use("/meals", mealsRouter);
 
+const knex = require("./database");
+
+//"/future-meals"	Respond with all meals in the future (relative to the when datetime)
+router.get("/future-meals", async (request, response) => {
+  try {
+    const futureMeals = await knex("meal").select("*").where('created_data', '>=', '2023-04-02');
+    response.json(futureMeals);
+  }
+  catch (error) {
+    throw error;
+  }
+});
+
+//"/past-meals"	Respond with all meals in the past (relative to the when datetime)
+
+router.get("/past-meals", async (request, response) => {
+  try {
+    const pastMeals = await knex("meal").select("*").where('created_data', '<=', '2023-04-02');
+    response.json(pastMeals);
+  }
+  catch (error) {
+    throw error;
+  }
+});
+
+//"/all-meals"	Respond with all meals sorted by ID
+
+router.get("/all-meals", async (request, response) => {
+  try {
+    const allMeals = await knex("meal").select("*").orderBy('id', 'desc');
+    response.json(allMeals);
+  }
+  catch (error) {
+    throw error;
+  }
+});
+
+//"/first-meal"	Respond with the first meal (meaning with the minimum id)
+
+router.get("/first-meal", async (request, response) => {
+  try {
+    const minimumId = await knex("meal").min('id as minId');
+    const firstMeal = await knex("meal").select("*").where('id', minimumId[0].minId);
+    if (firstMeal.length === 0) {
+      response.status(404).and("Meals not founded");
+    } else {
+      response.json(firstMeal);
+    }
+  }
+  catch (error) {
+    throw error;
+  }
+});
+
+//"/last-meal"	Respond with the last meal (meaning with the maximum id)
+
+router.get("/last-meal", async (request, response) => {
+  try {
+    const maximumId = await knex("meal").min('id as maxId');
+    const lastMeal = await knex("meal").select("*").where('id', maximumId[0].maxId);
+    if (lastMeal.length === 0) {
+      response.status(404).and("Meals not founded");
+    } else {
+      response.json(lastMeal);
+    }
+  }
+  catch (error) {
+    throw error;
+  }
+});
+
 if (process.env.API_PATH) {
   app.use(process.env.API_PATH, router);
 } else {
